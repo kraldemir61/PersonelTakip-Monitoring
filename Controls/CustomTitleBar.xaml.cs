654248cfeaf26c5a69619794bd39b97d10ef215b
kraldemir61 <kraldemir61@users.noreply.github.com>
2026-04-24 16:44:51 +0500

    UI Modernization: Dark Theme for dialogs, Logout button relocated, Notification popup fixed, Sidebar narrowed

---
+++ Controls/CustomTitleBar.xaml.cs
@@ -0,0 +1,69 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PersonelTakip.Controls;
+
+public partial class CustomTitleBar : UserControl
+{
+    public static readonly DependencyProperty AdditionalContentProperty =
+        DependencyProperty.Register("AdditionalContent", typeof(object), typeof(CustomTitleBar), new PropertyMetadata(null));
+
+    public object AdditionalContent
+    {
+        get { return GetValue(AdditionalContentProperty); }
+        set { SetValue(AdditionalContentProperty, value); }
+    }
+
+    public static readonly DependencyProperty CanMaximizeProperty =
+        DependencyProperty.Register("CanMaximize", typeof(bool), typeof(CustomTitleBar), new PropertyMetadata(true));
+
+    public bool CanMaximize
+    {
+        get { return (bool)GetValue(CanMaximizeProperty); }
+        set { SetValue(CanMaximizeProperty, value); }
+    }
+
+    public CustomTitleBar()
+    {
+        InitializeComponent();
+        Loaded += CustomTitleBar_Loaded;
+    }
+
+    private void CustomTitleBar_Loaded(object sender, RoutedEventArgs e)
+    {
+        var window = Window.GetWindow(this);
+        if (window != null)
+        {
+            window.StateChanged += (s, ev) => 
+            {
+                if (window.WindowState == WindowState.Maximized)
+                    MaximizeButton.Content = "\xE923"; // Restore icon
+                else
+                    MaximizeButton.Content = "\xE922"; // Maximize icon
+            };
+        }
+    }
+
+    private void Minimize_Click(object sender, RoutedEventArgs e)
+    {
+        var window = Window.GetWindow(this);
+        if (window != null) window.WindowState = WindowState.Minimized;
+    }
+
+    private void Maximize_Click(object sender, RoutedEventArgs e)
+    {
+        var window = Window.GetWindow(this);
+        if (window != null)
+        {
+            window.WindowState = window.WindowState == WindowState.Maximized 
+                ? WindowState.Normal 
+                : WindowState.Maximized;
+        }
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+        var window = Window.GetWindow(this);
+        window?.Close();
+    }
+}
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 using System.Windows;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+using System;
 using PersonelTakip.ViewModels;
 
 namespace PersonelTakip.Views;
@@ -14,5 +17,77 @@ public partial class MainWindow : Window
         {
             await viewModel.LoadAllDataAsync();
         };
+        SourceInitialized += MainWindow_SourceInitialized;
+    }
+
+    private void MainWindow_SourceInitialized(object? sender, EventArgs e)
+    {
+        nint handle = new WindowInteropHelper(this).Handle;
+        HwndSource.FromHwnd(handle)?.AddHook(WindowProc);
+    }
+
+    private nint WindowProc(nint hwnd, int msg, nint wParam, nint lParam, ref bool handled)
+    {
+        const int WM_GETMINMAXINFO = 0x0024;
+        if (msg == WM_GETMINMAXINFO)
+        {
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO))!;
+            nint monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+            if (monitor != IntPtr.Zero)
+            {
+                MONITORINFO monitorInfo = new MONITORINFO();
+                GetMonitorInfo(monitor, monitorInfo);
+                RECT rcWorkArea = monitorInfo.rcWork;
+                RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
+            }
+            Marshal.StructureToPtr(mmi, lParam, true);
+            handled = true;
+        }
+        return IntPtr.Zero;
+    }
+
+    private const int MONITOR_DEFAULTTONEAREST = 0x00000002;
+
+    [DllImport("user32.dll")]
+    private static extern nint MonitorFromWindow(nint handle, int flags);
+
+    [DllImport("user32.dll")]
+    private static extern bool GetMonitorInfo(nint hMonitor, MONITORINFO lpmi);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int x;
+        public int y;
+        public POINT(int x, int y) { this.x = x; this.y = y; }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MINMAXINFO
+    {
+        public POINT ptReserved;
+        public POINT ptMaxSize;
+        public POINT ptMaxPosition;
+        public POINT ptMinTrackSize;
+        public POINT ptMaxTrackSize;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public class MONITORINFO
+    {
+        public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+        public RECT rcMonitor = new RECT();
+        public RECT rcWork = new RECT();
+        public int dwFlags = 0;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int left, top, right, bottom;
     }
 }
